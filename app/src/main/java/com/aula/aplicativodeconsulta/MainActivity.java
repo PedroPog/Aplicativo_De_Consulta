6a2c28ec2d342092a7b0459fee37695cb2fbea16
Pedro Vieira <80628417+PedroPog@users.noreply.github.com>
2023-10-20 12:12:51 -0300

    Snack

--- app/src/main/java/com/aula/aplicativodeconsulta/MainActivity.java
+++ app/src/main/java/com/aula/aplicativodeconsulta/MainActivity.java
@@ -3,12 +3,46 @@ package com.aula.aplicativodeconsulta;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    ImageView imageLogo;
+    Button buttonLogin,buttonCadastro;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        carregarXml();
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toast("Button Login");
+            }
+        });
+        buttonCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toast("Button Cadastro");
+            }
+        });
+    }
+
+    private void carregarXml() {
+        imageLogo = findViewById(R.id.imageLogo);
+        buttonCadastro = findViewById(R.id.buttonCadastro);
+        buttonLogin = findViewById(R.id.buttonLogin);
+    }
+    private void toast(String msg){
+        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER | Gravity.CENTER, 0, 0);
+        toast.show();
     }
 }
