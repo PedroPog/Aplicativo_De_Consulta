7df291b068bfaa9f32e359022e0dadefff09ba41
Luis Felipe <86193136+luis-felipee@users.noreply.github.com>
2023-11-08 12:25:03 -0300

    Add files via upload
    tela login

---
+++ java/com/aula/aplicativodeconsulta/MainActivity.java
@@ -0,0 +1,53 @@
+package com.aula.aplicativodeconsulta;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+public class MainActivity extends AppCompatActivity {
+
+    ImageView imageLogo;
+    Button buttonLogin,buttonCadastro;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        carregarXml();
+        buttonLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toast("Button Login");
+            }
+        });
+        buttonCadastro.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                toast("Button Cadastro");
+
+                Intent intent = new Intent(MainActivity.this, CadastroActivity.class);
+                startActivity(intent);
+
+            }
+        });
+    }
+
+    private void carregarXml() {
+        imageLogo = findViewById(R.id.imageLogo);
+        buttonCadastro = findViewById(R.id.buttonCadastro);
+        buttonLogin = findViewById(R.id.buttonLogin);
+    }
+    private void toast(String msg){
+        Toast toast = Toast.makeText(this, msg, Toast.LENGTH_LONG);
+        toast.setGravity(Gravity.CENTER | Gravity.CENTER, 0, 0);
+        toast.show();
+    }
+}
