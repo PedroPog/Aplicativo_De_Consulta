4bc5babd8c1c65f38b4fbbb074d11831f5fbc358
Pedro Vieira <80628417+PedroPog@users.noreply.github.com>
2023-11-17 17:12:57 -0300

    Primeira tela

--- app/src/main/java/com/aula/aplicativodeconsulta/activity/PrimeiraActivity.java
+++ app/src/main/java/com/aula/aplicativodeconsulta/activity/PrimeiraActivity.java
@@ -5,19 +5,27 @@ import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.navigation.ui.NavigationUI;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Toast;
 
 import com.aula.aplicativodeconsulta.R;
 import com.aula.aplicativodeconsulta.databinding.ActivityMainBinding;
 import com.aula.aplicativodeconsulta.databinding.ActivityPrimeiraBinding;
+import com.aula.aplicativodeconsulta.recycler.RecyclerViewNomes;
+
+import java.util.ArrayList;
 
 public class PrimeiraActivity extends AppCompatActivity {
     private ActivityPrimeiraBinding binding;
 
     private NavHostFragment navHostFragment;
     private NavController navController;
+    RecyclerViewNomes adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,7 +35,6 @@ public class PrimeiraActivity extends AppCompatActivity {
         initNav();
 
     }
-
 
     private void initNav(){
         navHostFragment = (NavHostFragment) getSupportFragmentManager().findFragmentById(R.id.nav_host_fragment);
